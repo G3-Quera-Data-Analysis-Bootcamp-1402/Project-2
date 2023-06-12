88486096a00e41b3deba143799141b7f05a69fdb
ilia4bit <iliark82@gmail.com>
2023-06-12 17:54:37 +0330

    remove functions call

--- src/superstore/cleanizers/product_dim.py
+++ src/superstore/cleanizers/product_dim.py
@@ -6,5 +6,3 @@ def product_dim_cleanizer():
     df: pd.DataFrame = pd.read_csv(DATA_DIR / "product_dim.csv", delimiter="\t")
     df = pd.get_dummies(df, columns=["Sub-Category","Category"])
     return df
-
-print(product_dim_cleanizer())
