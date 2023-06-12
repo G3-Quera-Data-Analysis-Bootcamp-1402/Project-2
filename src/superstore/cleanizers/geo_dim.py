ab1fdcf676ab56e5608fe2999e12ea01ef158054
MoeinMirbaha <mirbahamoein98@gmail.com>
2023-06-12 17:06:04 +0330

    add geo_dim cleanizer script

---
+++ src/superstore/cleanizers/geo_dim.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+from superstore.consts import DATA_DIR
+
+
+def geo_dim_cleanizer():
+    df: pd.DataFrame = pd.read_csv(DATA_DIR / "geo_dim.csv", delimiter="\t")
+    df = pd.get_dummies(df, columns=["Country"], prefix= "Country")
+    return df
