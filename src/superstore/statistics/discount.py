fde09503f988c985d55d0e258feb23298f2b5ac3
ilia4bit <iliark82@gmail.com>
2023-06-15 20:14:55 +0330

    enhance - fix pandas error, add store func, rename variables

--- src/superstore/statistics/discount.py
+++ src/superstore/statistics/discount.py
@@ -3,40 +3,63 @@ import pathlib
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import scipy as sp
 from matplotlib.colors import LogNorm
-from scipy import stats
 
 
 from superstore.consts import DATA_DIR
 
 
-def divide_data(data_dir: pathlib.Path = DATA_DIR) -> None:
+def divide_data(data_dir: pathlib.Path = DATA_DIR) -> tuple[pd.DataFrame]:
+    """
+    merge fact table with product_dim table
+    extract needed columns
+    split data into 2 groups discounted and undiscounted
+    """
+
+    def discount_preprocessor(df: pd.DataFrame, discounted: bool) -> pd.DataFrame:
+        result_df: pd.DataFrame = df.loc[df["Discounted"] == discounted].copy().groupby("Product Name").sum(numeric_only=True).reset_index()
+        result_df["Product Code"] = result_df.index.values
+        cols: list[str] = ["Product Code", "Product Name", "Sales", "Quantity", "Discount", "Profit"]
+        return result_df.loc[:, cols]
+
+
     fact_df: pd.DataFrame = pd.read_csv(data_dir / "fact.csv", delimiter="\t")
-    fact_df["Discounted"] = (
-        fact_df["Discount"]
-        .mask(fact_df["Discount"] > 0, True)
-        .mask(fact_df["Discount"] == 0, False)
-    )
     product_df: pd.DataFrame = pd.read_csv(data_dir / "product_dim.csv", delimiter="\t")
-    df: pd.DataFrame = pd.merge(fact_df, product_df, how="left", on="Product ID").loc[
-        :, ["Product Name", "Sales", "Quantity", "Discount", "Profit", "Discounted"]
-    ]
-    discounted_df: pd.DataFrame = df.loc[df["Discounted"] == True]
-    undiscounted_df: pd.DataFrame = df.loc[df["Discounted"] == False]
-    discounted_product_df: pd.DataFrame = (
-        discounted_df.groupby("Product Name").sum(numeric_only=True).reset_index()
-    )
-    undiscounted_product_df = pd.DataFrame = (
-        undiscounted_df.groupby("Product Name").sum(numeric_only=True).reset_index()
-    )
-    discounted_product_df["Product Code"] = discounted_product_df.index.tolist()
-    undiscounted_product_df["Product Code"] = undiscounted_product_df.index.tolist()
-    return discounted_product_df, undiscounted_product_df
+    merged_df: pd.DataFrame = pd.merge(fact_df, product_df, how="left", on="Product ID")
+    merged_df["Discounted"] = merged_df["Discount"].mask(lambda x: x > 0, True).mask(lambda x: x == 0, False)
+    d_df: pd.DataFrame = discount_preprocessor(merged_df, True)
+    und_df: pd.DataFrame = discount_preprocessor(merged_df, False)
+    return (d_df, und_df)
+   
+
+def ttest_data(d_df: pd.DataFrame, und_df: pd.DataFrame) -> pd.DataFrame:
+    """
+    do ttest between discounted and undiscounted products using three metircs
+    sales, quantity, profit
+    """
+    metrics: list[str] = ["Sales", "Quantity", "Profit"]
+    d_metrics_df = d_df.loc[:, metrics].copy()
+    und_metrics_df = und_df.loc[:, metrics].copy()
+    
+    stats_values, p_values = sp.stats.ttest_ind(d_metrics_df, und_metrics_df)
+
+    data: dict[str, list] = {"Stats": stats_values.tolist(), "P_value": p_values.tolist()}
+
+    ttest_df: pd.DataFrame = pd.DataFrame(data, index=metrics)
+    ttest_df.index.name = "Metric"
+
+    return ttest_df
 
 
-def plot_data(data_dir: pathlib.Path = DATA_DIR) -> None:
-    d_df, und_df = divide_data(data_dir)
-
+def plot_data(d_df: pd.DataFrame, und_df: pd.DataFrame) -> None:
+    """
+    d_df: discounted products dataframe
+    und_df: undiscounted products dataframe
+    create 3 2D histogram plots for each groups (discounted, undiscounted)
+    x-axis: Product Code
+    y-axis: [Sales, Quantity, Profit]
+    """
     def get_values(df: pd.DataFrame, col: str) -> np.ndarray:
         return df[col].values
 
@@ -65,14 +88,11 @@ def plot_data(data_dir: pathlib.Path = DATA_DIR) -> None:
     axes[2][1].set_ylim(-10000, 20000)
     # show plots
     plt.show()
-    
-    
-def ttest(data_dir: pathlib.Path = DATA_DIR) -> tuple[np.ndarray]:
-    discounted_df, undiscounted_df = divide_data(data_dir)
 
-    discounted_metrics = discounted_df.loc[:, ["Sales", "Quantity", "Profit"]]
-    undiscounted_metrics = undiscounted_df.loc[:, ["Sales", "Quantity", "Profit"]]
-    
-    tstats, pvalue = stats.ttest_ind(discounted_metrics, undiscounted_metrics)
-    
-    return tstats, pvalue
+def store_data(dataframes: list[pd.DataFrame], filenames: list[str], has_index: list[bool], data_dir: pathlib.Path = DATA_DIR,) -> None:
+    """
+    store divide_data and ttest_data into csv files which stored in
+    data/statistics/
+    """
+    for df, fn, hi in zip(dataframes, filenames, has_index):
+        df.to_csv(data_dir / f"statistics/{fn}.csv", index=hi)
